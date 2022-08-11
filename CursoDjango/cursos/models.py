0f35b4002dd723a47046db1638387149c26017f3
Isaias0927 <107389961+Isaias0927@users.noreply.github.com>
2022-08-10 22:44:14 -0500

    Agregando proyecto cursos

--- CursoDjango/cursos/models.py
+++ CursoDjango/cursos/models.py
@@ -4,11 +4,11 @@ from django.db import models
 # Create your models here.
 class Cursos(models.Model):
     i = models.AutoField(primary_key=True, verbose_name="Clave")
-    nombre = models.CharField(max_length=100,verbose_name="Nombre completo")
-    curso = models.CharField(max_length=100,verbose_name="Nombre del curso")
+    nombre = models.TextField(max_length=20,verbose_name="Nombre completo")
+    curso = models.TextField(max_length=20,verbose_name="Nombre del curso")
     imagen = models.ImageField(null=True, upload_to="fotos", verbose_name="Fotografía")
     email = models.EmailField(max_length=200,verbose_name="Correo Electrónico")
-    tiempo = models.TextField(max_length=100,verbose_name="Descripcion")
+    tiempo = models.TextField(verbose_name="Descripcion")
     fecha = models.DateField(
         default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)#Fecha y tiempo
